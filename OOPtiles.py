c2dd223fb679783ec1baa9ef148815011ac814e4
Jared Lubich <154639972+ImThePiGuy@users.noreply.github.com>
2025-03-20 14:14:47 -0500

    OOPtiles.py

--- OOPtiles.py
+++ OOPtiles.py
@@ -17,15 +17,30 @@ for their connections.
 
 """
 
-import tkinter as tk
-
-
-
-
 class Tile:
-    def __init__(self, xpos, ypos):
+    def __init__(self,xpos,ypos):
         self.xpos = xpos
         self.ypos = ypos
+        
+    def __str__(self):
+        return f"X:{self.xpos},Y:{self.ypos}"
+        
+class Board:
+    
+    def __init__(self):
+        self.tiles = []
+        self.genTiles()
+        self.adjTiles()
 
-    def Draw(self):
-        #use xpos & ypos
+    def genTiles(self):
+        for x in range(7):
+            self.tiles.append([])
+            for y in range(5):
+                self.tiles[x].append(str(Tile(x, y)))
+                
+    def adjTiles(self):
+        for tile in self.tiles:
+            print(self.tiles[tile])
+        
+My_Board = Board()
+#print(My_Board.tiles)
